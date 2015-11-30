2d086243c09efc455143a15897c3c68562cb7d58
Jeff <jeff@jeffpuckett.com>
2015-11-29 21:39:31 -0600

    add extensions array for file comparison

--- Sitemap/rudi.php
+++ Sitemap/rudi.php
@@ -15,6 +15,7 @@ $path = realpath($path_real_relative_root);
 // http://stackoverflow.com/questions/2528848/recursion-through-a-directory-tree-in-php
 $dir = realpath($path_real_relative_root) . "/";
 function recurseDirs($main, $count=0){
+    $extensions = array("php", "html");
     $dirHandle = opendir($main);
     while($file = readdir($dirHandle)){
         if(is_dir($main.$file."/") && $file != '.' && $file != '..' && $file != '.git' && $file != '_resources'){
@@ -22,10 +23,11 @@ function recurseDirs($main, $count=0){
             $count = recurseDirs($main.$file."/",$count); // Correct call and fixed counting
         }
         else{
-	  if( $file != '.' && $file != '..' ){
-            $count++;
-            echo "$count: filename: $file in $main \n<br />";
-          }
+	    $ext = pathinfo($main.$file, PATHINFO_EXTENSION);
+	    if (in_array($ext,$extensions)){
+	      $count++;
+	      echo "$count: filename: $file in $main \n<br />";
+	    }
         }
     }
     return $count;
