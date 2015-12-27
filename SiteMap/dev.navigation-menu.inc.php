cbbf6303081c390a9a55b60405c8ed4835bafc5d
Jeff Puckett <jeff@jeffpuckett.com>
2015-12-26 21:37:47 -0600

    add glyphicons for menu item toggle

--- SiteMap/dev.navigation-menu.inc.php
+++ SiteMap/dev.navigation-menu.inc.php
@@ -3,7 +3,7 @@
 echo "
 
   <li><a href='$path_web_root/SubTree/'>SubTree</a></li>
-  <li><a href='$path_web_root/SiteMap/'>SiteMap</a> <a href='javascript:$(\"#sitemap\").show()'>Expand</a>
+  <li><a href='$path_web_root/SiteMap/'>SiteMap</a> <a href='javascript:void(0)' onclick='toggle_nav_item($(this))'><span class='navigation_menu_toggle glyphicon glyphicon-plus-sign'></span></a>
     <ul id='sitemap' style='display:none'>
       <li><a href='$path_web_root/SiteMap/iter.php'>Iterator</a></li>
     </ul>
@@ -16,6 +16,23 @@ echo "
   <li><a href='$path_web_root/FancyBox/'>FancyBox</a></li>
   <li><a href='$path_web_root/Embed/'>Embed</a></li>
 
+
+
 ";
 
 ?>
+
+<script>
+  function toggle_nav_item(toggle){
+    toggle.find(".glyphicon").toggleClass("glyphicon-plus-sign glyphicon-minus-sign");
+    toggle.parent().find("ul").toggle("blind");
+  }
+</script>
+
+<style>
+  .navigation_menu_toggle {
+    margin-top : 20px;
+    margin-right : 10px;
+    float : right;
+  }
+</style>
