758234220256ef3e28ce745f244ac23357a099d5
Jeff Puckett <jeff@jeffpuckett.com>
2015-12-28 10:53:12 -0600

    add index back to loop

--- SiteMap/sitemap.exclude.lst.update.ajax.php
+++ SiteMap/sitemap.exclude.lst.update.ajax.php
@@ -6,6 +6,7 @@ if (isset($_GET['exclude_file'])) {
   $exclude_list = file('sitemap.exclude.lst',FILE_IGNORE_NEW_LINES);
 
   // iterate through list of excluded items
+  $i = 0;
   foreach ($exclude_list as $item){
     // if removing file from list
     if ( $_GET["exclude_file"] == $item ){
