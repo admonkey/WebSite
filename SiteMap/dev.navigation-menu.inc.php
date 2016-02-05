0fd5578a8f4991c0064508924bf7be4ab2033937
Jeff Puckett <jeff@jeffpuckett.com>
2016-02-04 21:33:55 -0600

    rearrange nav menu

--- SiteMap/dev.navigation-menu.inc.php
+++ SiteMap/dev.navigation-menu.inc.php
@@ -65,9 +65,13 @@
 		  </li>
                 -->
                 
+                <!-- removed redundant sidebar brand
+		  <li class="sidebar-brand">
+		      <a href="/WebSite-template/">Web Site</a>
+		  </li>
+                -->
+                
       <li><a class="li_section_title page_link" href="/WebSite-template/SiteMap">SiteMap</a>
-
-</li><li><a class="li_section_title page_link" href="/WebSite-template/Profiles">Profiles</a>
 
 </li><li><a class="toggle_a" href="javascript:void(0)" onclick="toggle_nav_item($(this))"><span class="navigation_menu_toggle glyphicon glyphicon-plus-sign"></span></a><a class="li_section_title page_link" href="/WebSite-template/Login">Login</a>
 
@@ -106,7 +110,9 @@
 	      }
 	    </style>
 
-        </li><li><a class="toggle_a" href="javascript:void(0)" onclick="toggle_nav_item($(this))"><span class="navigation_menu_toggle glyphicon glyphicon-plus-sign"></span></a><a class="li_section_title page_link" href="/WebSite-template/Forum">Forum</a>
+        </li><li><a class="li_section_title page_link" href="/WebSite-template/Profiles">Profiles</a>
+
+</li><li><a class="toggle_a" href="javascript:void(0)" onclick="toggle_nav_item($(this))"><span class="navigation_menu_toggle glyphicon glyphicon-plus-sign"></span></a><a class="li_section_title page_link" href="/WebSite-template/Forum">Forum</a>
 
 <ul style="display: none;"><li><a class="page_link" href="/WebSite-template/Forum/rules.php">Rules</a></li></ul></li><li><a class="li_section_title page_link" href="/WebSite-template/Forms">Forms</a>
 
@@ -126,4 +132,4 @@
 
 
 
-                                                                                                                
+                                                                                                                            
